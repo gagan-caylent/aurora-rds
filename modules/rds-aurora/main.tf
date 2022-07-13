c6aa5a0467b0c2bca77cecae0f16141b424a600e
gagan-caylent <gaganpreet.singh@caylent.com>
2022-07-13 20:46:40 -0300

    first commit

---
+++ modules/rds-aurora/main.tf
@@ -0,0 +1,100 @@
+resource "aws_rds_cluster" "this" {
+  count = local.create_cluster ? 1 : 0
+
+  # Notes:
+  # iam_roles has been removed from this resource and instead will be used with aws_rds_cluster_role_association below to avoid conflicts per docs
+
+  global_cluster_identifier      = var.global_cluster_identifier
+  enable_global_write_forwarding = var.enable_global_write_forwarding
+  cluster_identifier             = var.name
+  replication_source_identifier  = var.replication_source_identifier
+  source_region                  = var.source_region
+
+  engine                              = var.engine
+  engine_mode                         = var.engine_mode
+  engine_version                      = local.is_serverless ? null : var.engine_version
+  allow_major_version_upgrade         = var.allow_major_version_upgrade
+  enable_http_endpoint                = var.enable_http_endpoint
+  kms_key_id                          = var.kms_key_id
+  database_name                       = var.is_primary_cluster ? var.database_name : null
+  master_username                     = var.is_primary_cluster ? var.master_username : null
+  master_password                     = var.is_primary_cluster ? local.master_password : null
+  final_snapshot_identifier           = "${var.final_snapshot_identifier_prefix}-${var.name}-${element(concat(random_id.snapshot_identifier.*.hex, [""]), 0)}"
+  skip_final_snapshot                 = var.skip_final_snapshot
+  deletion_protection                 = var.deletion_protection
+  backup_retention_period             = var.backup_retention_period
+  preferred_backup_window             = local.is_serverless ? null : var.preferred_backup_window
+  preferred_maintenance_window        = local.is_serverless ? null : var.preferred_maintenance_window
+  port                                = local.port
+  db_subnet_group_name                = local.db_subnet_group_name
+  vpc_security_group_ids              = compact(concat(aws_security_group.this.*.id, var.vpc_security_group_ids))
+  snapshot_identifier                 = var.snapshot_identifier
+  storage_encrypted                   = var.storage_encrypted
+  apply_immediately                   = var.apply_immediately
+  db_cluster_parameter_group_name     = var.db_cluster_parameter_group_name
+  db_instance_parameter_group_name    = var.allow_major_version_upgrade ? var.db_cluster_db_instance_parameter_group_name : null
+  iam_database_authentication_enabled = var.iam_database_authentication_enabled
+  backtrack_window                    = local.backtrack_window
+  copy_tags_to_snapshot               = var.copy_tags_to_snapshot
+  enabled_cloudwatch_logs_exports     = var.enabled_cloudwatch_logs_exports
+
+  timeouts {
+    create = lookup(var.cluster_timeouts, "create", null)
+    update = lookup(var.cluster_timeouts, "update", null)
+    delete = lookup(var.cluster_timeouts, "delete", null)
+  }
+
+  dynamic "scaling_configuration" {
+    for_each = length(keys(var.scaling_configuration)) == 0 || !local.is_serverless ? [] : [var.scaling_configuration]
+
+    content {
+      auto_pause               = lookup(scaling_configuration.value, "auto_pause", null)
+      max_capacity             = lookup(scaling_configuration.value, "max_capacity", null)
+      min_capacity             = lookup(scaling_configuration.value, "min_capacity", null)
+      seconds_until_auto_pause = lookup(scaling_configuration.value, "seconds_until_auto_pause", null)
+      timeout_action           = lookup(scaling_configuration.value, "timeout_action", null)
+    }
+  }
+
+  dynamic "serverlessv2_scaling_configuration" {
+    for_each = length(keys(var.serverlessv2_scaling_configuration)) == 0 || local.is_serverless ? [] : [var.serverlessv2_scaling_configuration]
+
+    content {
+      max_capacity = serverlessv2_scaling_configuration.value.max_capacity
+      min_capacity = serverlessv2_scaling_configuration.value.min_capacity
+    }
+  }
+  dynamic "s3_import" {
+    for_each = var.s3_import != null && !local.is_serverless ? [var.s3_import] : []
+    content {
+      source_engine         = "mysql"
+      source_engine_version = s3_import.value.source_engine_version
+      bucket_name           = s3_import.value.bucket_name
+      bucket_prefix         = lookup(s3_import.value, "bucket_prefix", null)
+      ingestion_role        = s3_import.value.ingestion_role
+    }
+  }
+
+  dynamic "restore_to_point_in_time" {
+    for_each = length(keys(var.restore_to_point_in_time)) == 0 ? [] : [var.restore_to_point_in_time]
+
+    content {
+      source_cluster_identifier  = restore_to_point_in_time.value.source_cluster_identifier
+      restore_type               = lookup(restore_to_point_in_time.value, "restore_type", null)
+      use_latest_restorable_time = lookup(restore_to_point_in_time.value, "use_latest_restorable_time", null)
+      restore_to_time            = lookup(restore_to_point_in_time.value, "restore_to_time", null)
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      # See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_cluster#replication_source_identifier
+      # Since this is used either in read-replica clusters or global clusters, this should be acceptable to specify
+      replication_source_identifier,
+      # See docs here https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/rds_global_cluster#new-global-cluster-from-existing-db-cluster
+      global_cluster_identifier,
+    ]
+  }
+
+  tags = merge(var.tags, var.cluster_tags)
+}
